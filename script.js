b6061b9f4ca86c53c4eb6d6d871fe6f37a7428c6
MulaHooky <110494010+MulaHooky@users.noreply.github.com>
2023-03-26 22:38:05 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('.btn-kontakt');
-const info = document.querySelector('.home-info');
-
-button.addEventListener('click', () => {
-        button.classList.toggle('active');
-        info.classList.toggle('active');
-    });
